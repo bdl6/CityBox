3d9287d7239f064c56292af780d70a9d88e0084b
bdl6 <163723192+bdl6@users.noreply.github.com>
2025-10-23 00:15:04 +0800

    diarychanged

--- pages/diary/diary.js
+++ pages/diary/diary.js
@@ -1,103 +1,69 @@
-// pages/diary/diary.js
+const app = getApp()
 Page({
   data: {
-    placeName: '',
-    photos: [],
-    description: '',
-    wordCount: 0,
-    canSubmit: false
+    imageList: [],
+    desc: '',
+    title:'',
+    descLength: 0
   },
-
-  onLoad(options) {
-    // 获取传入的地点名称
-    const placeName = options.placeName ? decodeURIComponent(options.placeName) : '';
-    this.setData({
-      placeName: placeName
-    });
-  },
-
-  // 上传照片
-  uploadPhoto() {
-    if (this.data.photos.length >= 3) {
-      wx.showToast({
-        title: '最多只能上传3张照片',
-        icon: 'none'
-      })
-      return
-    }
-
+onLoad(){
+  const title = app.globalData.selectedPOI.title
+  this.setData({
+    title
+  })
+}, 
+  // 选择图片
+  chooseImage() {
     wx.chooseImage({
-      count: 3 - this.data.photos.length,
-      sizeType: ['original', 'compressed'],
+      count: 3 - this.data.imageList.length,
+      sizeType: ['compressed'],
       sourceType: ['album', 'camera'],
       success: (res) => {
-        const newPhotos = this.data.photos.concat(res.tempFilePaths)
         this.setData({
-          photos: newPhotos
-        })
-        this.checkSubmitStatus()
+          imageList: this.data.imageList.concat(res.tempFilePaths)
+        });
       }
-    })
+    });
   },
-
-  // 删除照片
-  deletePhoto(e) {
-    const index = e.currentTarget.dataset.index
-    const photos = this.data.photos
-    photos.splice(index, 1)
+  // 删除图片
+  deleteImage(e) {
+    const index = e.currentTarget.dataset.index;
+    const newList = this.data.imageList.filter((_, i) => i !== index);
     this.setData({
-      photos: photos
-    })
-    this.checkSubmitStatus()
+      imageList: newList
+    });
   },
-
-  // 输入描述文字
-  onInput(e) {
-    const value = e.detail.value
+  // 输入描述
+  inputDesc(e) {
+    const desc = e.detail.value;
     this.setData({
-      description: value,
-      wordCount: value.length
-    })
-    this.checkSubmitStatus()
+      desc,
+      descLength: desc.length
+    });
   },
-
-  // 检查是否可以提交
-  checkSubmitStatus() {
-    const canSubmit = this.data.photos.length > 0 && 
-                      this.data.description.length >= 10 && 
-                      this.data.description.length <= 200
-    this.setData({
-      canSubmit: canSubmit
-    })
-  },
-
-  // 提交日记
-  submitDiary() {
-    if (!this.data.canSubmit) {
-      if (this.data.photos.length === 0) {
-        wx.showToast({
-          title: '请至少上传一张照片',
-          icon: 'none'
-        })
-      } else if (this.data.description.length < 10) {
-        wx.showToast({
-          title: '描述文字至少10个字',
-          icon: 'none'
-        })
-      }
-      return
+  // 发布
+  publish() {
+    if (this.data.imageList.length === 0) {
+      wx.showToast({
+        title: '请上传现场照片',
+        icon: 'none'
+      });
+      return;
     }
-
-    // 显示提交成功提示
+    if (this.data.desc.length < 10 || this.data.desc.length > 200) {
+      wx.showToast({
+        title: '请输入10-200字的感受',
+        icon: 'none'
+      });
+      return;
+    }
+    // 发布逻辑
     wx.showToast({
       title: '发布成功',
       icon: 'success'
-    })
-
-    // 模拟提交过程
+    });
     setTimeout(() => {
-      // 返回地点揭秘页并刷新UGC区
-      wx.navigateBack()
-    }, 1500)
+      wx.navigateBack();
+    }, 1500);
   }
-})
+});
